98699e6d8330570354dc248cfcaad0fccc93a4bb
유현호 <nolj126@gmail.com>
2024-02-15 20:41:51 +0900

    scripts.js => createMap.js 로 파일명 변경 + createMap.js 파일 수정 등등

--- public/js/createMap.js
+++ public/js/createMap.js
@@ -1,26 +1,29 @@
-function sendCoordinatesToBackend(latitude, longitude) {
-  // 좌표값을 서버로 전송하는 AJAX 요청
-  $.ajax({
-    type: 'POST', // HTTP 요청 방식: POST
-    url: '/unmatchedPath', // 요청을 보낼 URL
-    data: {
-      // 전송할 데이터
-      lat: latitude, // 위도
-      lng: longitude, // 경도
+const currentAddressSettingButton = document.getElementById('sendButton')
+function sendPost(coordinateData) {
+  fetch('/unmatchedPath', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-    success: function (response) {
-      // 요청 성공 시 실행되는 콜백 함수
-      console.log('좌표값을 성공적으로 전송했습니다.')
-    },
-    error: function (xhr, status, error) {
-      // 요청 실패 시 실행되는 콜백 함수
-      console.error('좌표값 전송에 실패했습니다:', error)
-    },
+    body: JSON.stringify(coordinateData),
   })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('unmatchedPath Model create Fail!')
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log('create Successful:', data)
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
 }
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
   mapOption = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    center: new kakao.maps.LatLng(37.2095934, 126.9817136), // 지도의 중심좌표
     level: 3, // 지도의 확대 레벨
   }
 var map = new kakao.maps.Map(mapContainer, mapOption) // 지도를 생성합니다
@@ -56,41 +59,11 @@ function init() {
       var lat = position.coords.latitude, // 위도
         lon = position.coords.longitude // 경도
 
-      const coordinateData = {
-        lat,
-        lng: lon,
-      }
-
-      fetch('/unmatchedPath', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(coordinateData),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('unmatchedPath Model create Fail!')
-          }
-          return response.json()
-        })
-        .then((data) => {
-          console.log('create Successful:', data)
-          // 로그인에 성공하면 필요한 작업을 수행합니다.
-        })
-        .catch((error) => {
-          console.error('Error:', error)
-          // 로그인에 실패하면 사용자에게 메시지를 표시하거나 다른 작업을 수행합니다.
-        })
-
       var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
         message = '<div style="padding:5px;">여기에 계신가요?!</div>' // 인포윈도우에 표시될 내용입니다
 
       // 마커와 인포윈도우를 표시합니다
       displayMarker(locPosition, message)
-
-      // 좌표를 백엔드로 전송합니다
-      sendCoordinatesToBackend(lat, lon)
     })
   } else {
     // HTML5의 GeoLocation을 사용할 수 없을 때 마커 표시 위치와 인포윈도우 내용을 설정합니다
@@ -102,3 +75,14 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init)
+currentAddressSettingButton.addEventListener('click', function () {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var lat = position.coords.latitude,
+      lng = position.coords.longitude
+    const coordinateData = {
+      lat,
+      lng,
+    }
+    sendPost(coordinateData)
+  })
+})
