207962a67a950625e583e80ddc7f399c2de2e77d
baecheolhyeon <todd1130@naver.com>
2024-03-21 23:22:58 +0900

    택시기사매칭 시작전

---
+++ migrations/1711028409561-migration.ts
@@ -0,0 +1,17 @@
+// import { MigrationInterface, QueryRunner } from 'typeorm'
+
+// export class migration1711028409561 implements MigrationInterface {
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.addColumn(
+//       'USER',
+//       new TableColumn({
+//         name: 'isMatching',
+//         type: 'boolean',
+//         isNullable: true,
+//         default: false,
+//       }),
+//     )
+//   }
+
+//   public async down(queryRunner: QueryRunner): Promise<void> {}
+// }
